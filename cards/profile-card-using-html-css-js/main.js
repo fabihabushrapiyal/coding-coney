fdf62f3445f781bd8a109e67b034ec37b6802dee
Piyal <fabihabushra124@gmail.com>
2025-04-11 14:53:01 +0600

    Added a new card project

--- cards/profile-card-using-html-css-js/main.js
+++ cards/profile-card-using-html-css-js/main.js
@@ -12,28 +12,3 @@ showHideBtn.addEventListener('click', () => {
     showHideBtn.innerHTML = '<i class="fa-solid fa-less-than"></i>';
   }
 });
-
-// HTML
-//  <h4>skills</h4>
-//           <div class="skill">
-//             <h6><i class="fa-brands fa-html5"></i> html5</h6>
-//             <div class="bar html">
-//               <p>95%</p>
-//             </div>
-//           </div>
-//           <div class="skill">
-//             <h6><i class="fa-brands fa-css3-alt"></i> css3</h6>
-//             <div class="bar css">
-//               <p>90%</p>
-//             </div>
-//           </div>
-//           <div class="skill">
-//             <h6><i class="fa-brands fa-js"></i> javascript</h6>
-//             <div class="bar js">
-//               <p>75%</p>
-//             </div>
-//           </div>
-
-{
-  /*  */
-}
